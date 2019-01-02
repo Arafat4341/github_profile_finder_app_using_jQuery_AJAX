8aeec9425dad491c374afe6ba4644426294dfd3b
Arafat4341 <mdyasinarafatyen2@gmail.com>
2019-01-01 20:52:08 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
 								<div class="col-md-3">
 									<span class="label label-default">Forks: ${repo.forks_count}</span>
 									<span class="label label-primary">Watchers: ${repo.watchers_count}</span>
-									<span class="label label-success">Stars: ${repo.stargazars_count}</span>
+									<span class="label label-success">Stars: ${repo.stargazers_count}</span>
 								</div>
 								<div class="col-md-2">
 									<a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo Page</a>
